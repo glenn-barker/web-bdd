8b79b47e01a810cca7254c40f7000679634125c9
robertgb <glenn.barker@spglobal.com>
2019-04-02 10:22:49 -0400

    Cleanup in browser fixtures.

--- features/environment.py
+++ features/environment.py
@@ -1,30 +1,11 @@
 import json
-from behave import fixture
 from behave.fixture import use_fixture_by_tag
 from features.fixtures import selenium_fixtures
 
 
-@fixture
-def selenium_browser(context):
-    # Using the `fixture.browser` fixture should do a lookup on the config and return an appropriate
-    # sub-fixture, e.g.: `fixture.browser.chrome`.
-    browser = context.config.userdata.get("browser", "chrome").lower()
-    fixture_tag = f"fixture.browser.{browser}"
-    try:
-        yield use_fixture_by_tag(fixture_tag, context, fixture_registry)
-    except LookupError:
-        raise ValueError(f"Unsupported browser option: {browser}")
-
-
-fixture_registry = {
-    # Map each valid `@fixture` tag with the appropriate test fixture.
-    "fixture.browser": selenium_browser,
-    "fixture.browser.chrome": selenium_fixtures.chrome,
-    "fixture.browser.edge": selenium_fixtures.edge,
-    "fixture.browser.firefox": selenium_fixtures.firefox,
-    "fixture.browser.ie": selenium_fixtures.ie,
-    "fixture.browser.safari": selenium_fixtures.safari,
-}
+# Map each valid `@fixture` tag with the appropriate test fixture.
+fixture_registry = {}
+fixture_registry.update(selenium_fixtures.fixture_registry)
 
 
 def before_all(context):
--- features/fixtures/selenium_fixtures.py
+++ features/fixtures/selenium_fixtures.py
@@ -1,7 +1,9 @@
 from behave import fixture
+from behave.fixture import use_fixture_by_tag
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.edge.options import Options as EdgeOptions
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.ie.options import Options as IEOptions
 from framework.selenium_helpers.selenium_wrapper import SeleniumWrapper
@@ -14,11 +16,14 @@ def chrome(context):
         chrome_options = ChromeOptions()
 
         if should_use_headless(context):
-            chrome_options.add_argument("--headless")
+            chrome_options.headless = True
 
+        # Launch Chrome browser with Selenium.
         driver = webdriver.Chrome(chrome_options=chrome_options)
+
+        # Store reference to Selenium wrapper in Scenario Context so all step definitions & page objects can use it.
         context.selenium = SeleniumWrapper(context, driver)
-        yield context.selenium.driver
+        yield context.selenium
     except WebDriverException as e:
         print("Selenium WebDriverException raised when initializing Chrome!")
         raise e
@@ -30,8 +35,18 @@ def chrome(context):
 def edge(context):
     # -- FIXTURE SETUP
     try:
-        context.selenium = SeleniumWrapper(context, webdriver.Edge())
-        yield context.selenium.driver
+        edge_options = EdgeOptions()
+
+        if should_use_headless(context):
+            print("WARNING: Headless execution is NOT supported by Edge!")
+            print("Ignoring headless flag and continuing execution with Edge in full UI mode.")
+
+        # Launch Edge browser with Selenium.
+        driver = webdriver.Edge(capabilities=edge_options.to_capabilities())
+
+        # Store reference to Selenium wrapper in Scenario Context so all step definitions & page objects can use it.
+        context.selenium = SeleniumWrapper(context, driver)
+        yield context.selenium
     except WebDriverException as e:
         print("Selenium WebDriverException raised when initializing Edge!")
         raise e
@@ -45,11 +60,15 @@ def firefox(context):
     try:
         firefox_options = FirefoxOptions()
 
-        firefox_options.headless = should_use_headless(context)
+        if should_use_headless(context):
+            firefox_options.headless = True
 
+        # Launch Firefox browser with Selenium.
         driver = webdriver.Firefox(options=FirefoxOptions)
+
+        # Store reference to Selenium wrapper in Scenario Context so all step definitions & page objects can use it.
         context.selenium = SeleniumWrapper(context, driver)
-        yield context.selenium.driver
+        yield context.selenium
     except WebDriverException as e:
         print("Selenium WebDriverException raised when initializing Firefox!")
         raise e
@@ -64,11 +83,15 @@ def ie(context):
         ie_options = IEOptions()
 
         if should_use_headless(context):
-            print("WARNING: Current config asks for both headless execution and IE execution, which are incompatible.")
+            print("WARNING: Headless execution is NOT supported by IE!")
+            print("Ignoring headless flag and continuing execution with IE in full UI mode.")
 
+        # Launch IE browser with Selenium.
         driver = webdriver.Ie(ie_options=ie_options)
+
+        # Store reference to Selenium wrapper in Scenario Context so all step definitions & page objects can use it.
         context.selenium = SeleniumWrapper(context, driver)
-        yield context.selenium.driver
+        yield context.selenium
     except WebDriverException as e:
         print("Selenium WebDriverException raised when initializing IE!")
         raise e
@@ -80,8 +103,16 @@ def ie(context):
 def safari(context):
     # -- FIXTURE SETUP
     try:
-        context.selenium = SeleniumWrapper(context, webdriver.Safari())
-        yield context.selenium.driver
+        if should_use_headless(context):
+            print("WARNING: Headless execution is NOT supported by Safari!")
+            print("Ignoring headless flag and continuing execution with Safari in full UI mode.")
+
+        # Launch Safari browser with Selenium.
+        driver = webdriver.Safari()
+
+        # Store reference to Selenium wrapper in Scenario Context so all step definitions & page objects can use it.
+        context.selenium = SeleniumWrapper(context, driver)
+        yield context.selenium
     except WebDriverException as e:
         print("Selenium WebDriverException raised when initializing Safari!")
         raise e
@@ -89,5 +120,29 @@ def safari(context):
     context.selenium.driver.quit()
 
 
+@fixture
+def selenium_browser(context):
+    # Using the `fixture.browser` fixture should do a lookup on the config and return an appropriate
+    # sub-fixture, e.g.: `fixture.browser.chrome`.
+    browser = context.config.userdata.get("browser", "chrome").lower()
+    fixture_tag = f"fixture.browser.{browser}"
+    try:
+        yield use_fixture_by_tag(fixture_tag, context, fixture_registry)
+    except LookupError:
+        raise ValueError(f"Unsupported browser option: {browser}")
+
+
+fixture_registry = {
+    # Map each valid `@fixture` tag with the appropriate test fixture.
+    "fixture.browser": selenium_browser,
+    "fixture.browser.chrome": chrome,
+    "fixture.browser.edge": edge,
+    "fixture.browser.firefox": firefox,
+    "fixture.browser.ie": ie,
+    "fixture.browser.safari": safari,
+}
+
+
 def should_use_headless(context):
+    # Poll either the cmd line args OR the specified JSON config to determine whether to use headless mode or not.
     return context.config.userdata.getbool("headless", False)
